8240d3bd546f2d12c94511a029f8cbf9a4b0d912
Paulljc <reborn_cheng@126.com>
2019-06-21 22:45:53 +0800

    complete code

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,32 @@
--- **数据库级别：**  
+﻿-- **数据库级别：**  
 --  显示所有数据库  
-
+show database;
 --  进入某个数据库  
-
+use student_examination_sys;
 --  创建一个数据库  
-
+create database if not exist myDemo1;
 --  创建指定字符集的数据库  
-
+create database if not exist myDemo2 default charset utf8;
 --  显示数据库的创建信息   
-
+show create database myDemo2;
 --  修改数据库的编码  
-
+alter database myDemo2 character set gbk;
 --  删除一个数据库   
-
+deop database myDemo2;
 -- **表级别**
 --  修改表名
-
+alter table subject rename to subjects;
 --  修改字段的数据类型
-
+alter table subjects modify teacher varchar(30);
 --  修改字段名
-
+alter table subjects change description descriptions varchar(30);
 --  添加字段
-
+alter table subjects add theme varchar(20);
 --  删除字段
-
+alter table subjects drop theme;
 --  修改表的存储引擎
-
+alter table subjects engine=MYISAM;
 --  删除表的外键约束
-
+alter table subjects drop foreign key student_id;
 --  删除一张表
+drop table subjects;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
--- 插入记录  
-
+﻿-- 插入记录  
+INSERT INTO `student` VALUES ('003', '王五', '18', '男');
 -- 修改记录  
-
+UPDATE student set name='吴五' WHERE id='003’;
 --  删除记录  
-
+DELETE FROM student WHERE id ='003';
 -- 查询记录  
+SELECT name from student where id='003';
